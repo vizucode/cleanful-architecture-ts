f8764f0ac94299748bb81c3d885e25966cfc27b3
vizucode <havisikkubaru@gmail.com>
2023-11-25 18:20:52 +0700

    feat: remove build folde

--- build/constants/error.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.ErrorConstant = void 0;
-exports.ErrorConstant = {
-    ErrNotFound: 404,
-    ErrNotFoundMsg: "Data tidak ditemukan.",
-    ErrInternalServer: 500,
-    ErrInternalServerMsg: "Kesalahan pada server."
-};
--- build/main.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_register_1 = require("./controller/express/express_register");
-const card_1 = require("./repositories/prisma/card");
-const card_2 = require("./usecases/card/card");
-// dependency injection
-const usecaseCard = new card_2.Uc_Card_Impl(new card_1.Prisma_Card_Impl());
-const rest = new express_register_1.Express_Register(usecaseCard);
-rest.Runner();
